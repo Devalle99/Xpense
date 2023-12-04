d78473d0adab0f6f15cfd31ec80230e575da8857
David Eduardo Vallejo Bravo <122117204+Devalle99@users.noreply.github.com>
2023-12-04 13:49:48 -0500

    Primera migración de la base
    Estuve copiando lo que el profe hizo en la clase 6 hasta la hora 1:20:00. Hice la primera migración pero tuve un error al tratar de hacer update

--- Proyecto/Xpense.domain/Expenses/Expense.cs
+++ Proyecto/Xpense.domain/Expenses/Expense.cs
@@ -7,6 +7,6 @@ namespace Xpense.domain.Expenses
         public required int Usuario { get; set; }
         public string Concepto { get; set; }
         public decimal Monto { get; set; }
-        public int Categoria { get; set; }
+        public int CategoriaId { get; set; }
     }
 }
---
+++ Proyecto/Xpense.infrastructure/Data/XpenseContext.cs
@@ -0,0 +1,31 @@
+﻿using Xpense.domain.Categories;
+using Xpense.domain.Expenses;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xpense.infrastructure.Data
+{
+    public class XpenseContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public XpenseContext(DbContextOptions<XpenseContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Expense>()
+                .Property(e => e.Monto)
+                .HasColumnType("decimal(18, 2)"); // 18 es la precisión total y 2 es la escala (número de decimales)
+        }
+    }
+}
