1e38fd49961fe626dad431e6f28981672ac6e462
David Eduardo Vallejo Bravo <122117204+Devalle99@users.noreply.github.com>
2024-03-09 14:57:53 -0500

    Renombrar carpetas

--- Proyecto/Frontend/src/Seguridad/PrivateRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, Navigate } from 'react-router-dom'
-import { useAuth } from '../Servicios/AuthService'
-
-function PrivateRoutes() {
-    const token = useAuth()
-    return token ? <Outlet /> : <Navigate to='/' />
-}
-
-export default PrivateRoutes
